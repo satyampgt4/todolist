09488b5c8632f006cc37f47ac8843a4f150d75de
satyampgt4 <satyamgpt44@gmail.com>
2021-04-19 15:37:07 +0530

    create to do list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [newtask, setnewtask] = useState();
+  const [task, settask] = useState([]);
+
+  const inputtask = (event) => {
+      setnewtask(event.target.value);
+  };
+
+  const addNewtask = () =>{
+      settask((oldtask) =>{
+        return[...oldtask,newtask];
+      });
+      setnewtask("");
+  };
+  const removetask = (id) =>{
+
+    settask((oldtask) =>{
+      return oldtask.filter((arrele, index)=>{
+        return index!= id;
+      }
+      );
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className = "container">
+        <div className = "containt">
+          <br/>
+          <h1>ToDo List</h1>
+          <br/>
+          <input type ="text" placeholder =" Add a Task" onChange = {inputtask} value ={newtask}/>
+
+            <i class="fas fa-plus-circle" onClick = {addNewtask}></i>
+          <ol>
+            {
+            task.map((taskvalue,index) =>{
+              return (<div className="task">
+                <i className="fas fa-times-circle"
+                  onClick = {() => removetask(index)}
+                  ></i>
+                <li>{taskvalue}</li>
+                </div>)
+              
+              ;
+            })
+          }
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
